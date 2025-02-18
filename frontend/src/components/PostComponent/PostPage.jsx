345b4447bfeca703a92e475446c4f0bf90f16075
das110722 <phuocvu.hcmc@gmail.com>
2025-02-18 15:34:01 +0700

    Create Post

--- frontend/src/components/PostComponent/PostPage.jsx
+++ frontend/src/components/PostComponent/PostPage.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import Input from '../Input/Input';
 import './PostPage.scss';
 
-const PostPage = () => {
+const PostPage = (props) => {
+    const { setPost } = props;
     const [title, setTitle] = useState('Add a new title');
     const [desc, setDesc] = useState('Add a new description');
     const [selectedIdx, setSelectedIdx] = useState(0);
     const tags = ['None', 'NSFW', 'Mood', 'Quotes', 'Shitpost'];
 
     const handlePost = () => {
-        // Post the data to the server
+        setPost(false);
+        const newPost = {
+            title: title,
+            description: desc,
+            tags: selectedIdx,
+        };
     };
     return (
         <section className="makepost-container ">
@@ -22,21 +29,21 @@ const PostPage = () => {
             <Input data={desc} inputType="textarea" setData={setDesc} label="Description" classStyle="makepost-desc" />
             <label> Tags </label>
             <div className="makepost-tags">
-                {tags.map((tag, idx) => {
-                    return (
-                        <>
-                            <button
-                                key={idx}
-                                className={`${selectedIdx === idx ? `makepost-tags-selected` : `makepost-tags-${tag}`}`}
-                                onClick={() => setSelectedIdx(idx)}>
-                                {tag}
-                            </button>
-                        </>
-                    );
-                })}
+                {tags.map((tag, idx) => (
+                    <button
+                        key={idx}
+                        className={`${selectedIdx === idx ? `makepost-tags-selected` : `makepost-tags-${tag}`}`}
+                        onClick={() => setSelectedIdx(idx)}>
+                        {tag}
+                    </button>
+                ))}
             </div>
         </section>
     );
+};
+
+PostPage.propTypes = {
+    setPost: PropTypes.func.isRequired,
 };
 
 export default PostPage;

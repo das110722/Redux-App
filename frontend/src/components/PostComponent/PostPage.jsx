1ad057a57659a74b7631b803774eef9f70971f31
das110722 <phuocvu.hcmc@gmail.com>
2025-02-18 15:22:56 +0700

    Làm sao để tag đổi màu khi click?

--- frontend/src/components/PostComponent/PostPage.jsx
+++ frontend/src/components/PostComponent/PostPage.jsx
@@ -5,20 +5,32 @@ import './PostPage.scss';
 const PostPage = () => {
     const [title, setTitle] = useState('Add a new title');
     const [desc, setDesc] = useState('Add a new description');
+    const [selectedIdx, setSelectedIdx] = useState(0);
     const tags = ['None', 'NSFW', 'Mood', 'Quotes', 'Shitpost'];
+
+    const handlePost = () => {
+        // Post the data to the server
+    };
     return (
         <section className="makepost-container ">
             <div className="makepost-navigation">
-                <p className="makepost-save">Post</p>
+                <p className="makepost-save" onClick={handlePost}>
+                    Post
+                </p>
             </div>
             <Input data={title} inputType="textarea" setData={setTitle} label="Title" classStyle="makepost-title" />
             <Input data={desc} inputType="textarea" setData={setDesc} label="Description" classStyle="makepost-desc" />
             <label> Tags </label>
             <div className="makepost-tags">
-                {tags.map((tag) => {
+                {tags.map((tag, idx) => {
                     return (
                         <>
-                            <button className={`makepost-tags-${tag}`}>{tag}</button>
+                            <button
+                                key={idx}
+                                className={`${selectedIdx === idx ? `makepost-tags-selected` : `makepost-tags-${tag}`}`}
+                                onClick={() => setSelectedIdx(idx)}>
+                                {tag}
+                            </button>
                         </>
                     );
                 })}

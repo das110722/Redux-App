a007ab73a88f802a90ae7feab98cdd89151830aa
das110722 <phuocvu.hcmc@gmail.com>
2025-02-18 15:48:33 +0700

    Tạo postSlice trong Redux Toolkit

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+import { postSlice } from './postSlice';
 
 export default configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        post: postSlice
     }
 })
